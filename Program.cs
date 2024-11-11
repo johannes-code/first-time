11e679dc00045677fd35103e804f7c8aabf221c8
johannes-code <johannes24sfj@gmail.com>
2024-11-11 12:23:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+string testing = "Testing testing";
+Console.WriteLine(testing);
+
+bool isEnabled = false;
+
+if (isEnabled)
+{
+    Console.WriteLine("Boolean is true");
+}
+
+int count = 0;
+while (count < 10)
+{
+    Console.WriteLine("Count: " + count);
+    count++;
+}
+
+Console.WriteLine("old breadfoot!");
+
+Person skrotnisse = new Person("Skrotnisse", "Rotehue");
+
+Console.WriteLine("The persons first name is: " + skrotnisse.firstName);
+Console.WriteLine("The persons last name is: " + skrotnisse.lastName);
+
+int numbersOfPlayers = 0;
+while (numbersOfPlayers < 6)
+{
+    Person person = new Person("Player", "Annon" + numbersOfPlayers);
+    Console.WriteLine("Player: " + person.lastName);
+    numbersOfPlayers++;
+}
---
+++ person.cs
@@ -0,0 +1,13 @@
+class Person
+{
+    //Datafields
+    public string firstName;
+    public string lastName;
+
+    //Constroctur, defines the logic for creating this object
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
